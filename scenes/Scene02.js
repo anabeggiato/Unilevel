e80dcec3c20e1541c3db65a144c6c9355d57c372
anabeggiato <ana.beggiato@sou.inteli.edu.br>
2024-03-05 10:29:52 -0300

    Initial commit

---
+++ scenes/Scene02.js
@@ -0,0 +1,25 @@
+var player;
+var plataforma;
+    
+
+class Scene02 extends Phaser.Scene{
+    
+    constructor(){
+        super({key: "Scene02"});
+        const config = {
+            
+            }
+        }
+        
+    
+    
+        preload(){
+     
+    }
+        create(){
+        
+    }
+        update(){
+
+    }
+}
